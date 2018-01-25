fc54bb73e4540152676fdb1a128ec35b4e72a5d8
Syed Ashique <syed.ashique07@gmail.com>
2018-01-25 10:49:51 -0500

    Add ButterKnife and cleanup the NewWordActivity.java

--- app/src/main/java/syed/com/wordrepo/activity/NewWordActivity.java
+++ app/src/main/java/syed/com/wordrepo/activity/NewWordActivity.java
@@ -3,59 +3,55 @@ package syed.com.wordrepo.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.greenrobot.eventbus.EventBus;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import syed.com.wordrepo.R;
-import syed.com.wordrepo.entitiy.Word;
 import syed.com.wordrepo.WordRepoApplication;
+import syed.com.wordrepo.entitiy.Word;
+import syed.com.wordrepo.utility.Log;
 
 public class NewWordActivity extends AppCompatActivity {
-    //Todo implement eventbus
+    //Todo implement eventbus need to cleanup
     public static final String EXTRA_REPLY = "NEW_WORD";
 
-    private EditText mEditWordView;
-    private EditText mEditMeaningView;
-    private EditText mEditDescriptionView;
-    private EditText mEditNoteView;
-    EventBus bus;
+    @BindView(R.id.edit_word)
+    public EditText mEditWordView;
+
+    @BindView(R.id.edit_meaning)
+    public EditText mEditMeaningView;
+
+    @BindView(R.id.edit_description)
+    public EditText mEditDescriptionView;
+
+    @BindView(R.id.edit_note)
+    public EditText mEditNoteView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
-        mEditWordView = findViewById(R.id.edit_word);
-        mEditMeaningView = findViewById(R.id.edit_meaning);
-        mEditDescriptionView = findViewById(R.id.edit_description);
-        mEditNoteView = findViewById(R.id.edit_note);
-//        final WordRepository wordRepository = new WordRepository(getApplication());
+        ButterKnife.bind(this);
+    }
 
+    @OnClick(R.id.button_save)
+    public void saveWord() {
+        Log.info(this.getClass(),"Clicked on the save button");
+        if (TextUtils.isEmpty(mEditWordView.getText())) {
+            Toast.makeText(getApplicationContext(),"Word is empty. Please add a word", Toast.LENGTH_SHORT).show();
+        } else {
+            Word word = new Word(mEditWordView.getText().toString()
+                    , mEditMeaningView.getText().toString()
+                    , mEditDescriptionView.getText().toString()
+                    , mEditNoteView.getText().toString());
 
-        final Button button = findViewById(R.id.button_save);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-
-
-                Log.d("syed :","Clicked on the save button");
-                if (TextUtils.isEmpty(mEditWordView.getText())) {
-                    Toast.makeText(getApplicationContext(),"text is empty", Toast.LENGTH_SHORT).show();
-                } else {
-                    Word word = new Word(mEditWordView.getText().toString()
-                            , mEditMeaningView.getText().toString()
-                            , mEditDescriptionView.getText().toString()
-                            , mEditNoteView.getText().toString());
-
-                    WordRepoApplication.getWordRepository().insertWord(word);
-//                    EventBus.getDefault().post(word);
-                }
-                finish();
-            }
-
-        });
+            //Todo need to work with it. Not good to expose repository in the whole application context
+            WordRepoApplication.getWordRepository().insertWord(word);
+            finish();
+//          EventBus.getDefault().post(word);
+        }
     }
 }
