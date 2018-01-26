1d907900856ec17dd238555405c366a928354c01
Syed Ashique <syed.ashique07@gmail.com>
2018-01-26 11:37:03 -0500

    cleanup the RecordActivity and added butterknife event

--- app/src/main/java/syed/com/wordrepo/activity/NewRecordActivity.java
+++ app/src/main/java/syed/com/wordrepo/activity/NewRecordActivity.java
@@ -10,31 +10,30 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import syed.com.wordrepo.R;
 import syed.com.wordrepo.utility.AudioFileUtil;
 
 public class NewRecordActivity extends AppCompatActivity {
-
-    private static final String TAG = "NewRecordActivity: syed";
     private static final int TIMER_RUNNING = 0;
     private static final int STOP_TIMER = 1;
-
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
-    private Button mRecordButton = null;
+    @BindView(R.id.timer_text)
+    public TextView mCurrentTimeTV;
+
     private MediaRecorder mRecorder = null;
-    private TextView mCurrentTimeTV = null;
     private int mCurrentTime;
     private boolean mStartRecording = false;
 
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
     private String [] permissions = {Manifest.permission.RECORD_AUDIO};
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -45,10 +44,39 @@ public class NewRecordActivity extends AppCompatActivity {
                 break;
         }
         if (!permissionToRecordAccepted ) {
-            Toast.makeText(getApplicationContext(), "No permission to record", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "No permission to record.\n Goto \"Settings -> App -> wordrepo\"." +
+                    "\nManually accept the mic permission first", Toast.LENGTH_LONG).show();
             finish();
         }
+    }
 
+    final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == TIMER_RUNNING) {
+                updateTimer();
+                sendEmptyMessageDelayed(TIMER_RUNNING, 1000);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record);
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        ButterKnife.bind(this);
+
+        mStartRecording = true;
+    }
+
+    //Todo need to handle the configuration change for mStartRecording variable
+    @OnClick(R.id.record_button)
+    public void onRecord(View v) {
+        onRecord(mStartRecording);
+        mStartRecording = !mStartRecording;
     }
 
     private void onRecord(boolean start) {
@@ -65,6 +93,12 @@ public class NewRecordActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        stopTimer();
+        super.onDestroy();
+    }
+
     private void startTimer() {
         mCurrentTime = 0 ;
         if (mHandler != null) {
@@ -78,77 +112,15 @@ public class NewRecordActivity extends AppCompatActivity {
         }
     }
 
-    final Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case TIMER_RUNNING:
-                    updateTimer();
-                    sendEmptyMessageDelayed(TIMER_RUNNING, 1000);
-                    break;
-                default:
-                    super.handleMessage(msg);
-                    break;
-            }
-        }
-    };
-
     private void updateTimer() {
         mCurrentTime++;
-        int hh = mCurrentTime/(3600);
-        int mm = (mCurrentTime-hh*3600)/60;
-        int ss = (mCurrentTime - (hh*3600) - (mm*60));
-        mCurrentTimeTV.setText(hh+" : "+mm+" : "+ss);
+        setmCurrentTime();
     }
 
-
-    @Override
-    protected void onDestroy() {
-        stopTimer();
-        super.onDestroy();
+    private void setmCurrentTime() {
+        int hh = mCurrentTime / (3600);
+        int mm = (mCurrentTime - (hh * 3600)) / 60;
+        int ss = (mCurrentTime - (hh * 3600) - (mm * 60));
+        mCurrentTimeTV.setText(hh + " : " + mm + " : " + ss);
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_record);
-
-        mRecordButton = (Button) findViewById(R.id.record_button);
-
-        mStartRecording = true;
-        mRecordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                mStartRecording = !mStartRecording;
-            }
-        });
-
-        mCurrentTimeTV= (TextView) findViewById(R.id.timer_text);
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-
-    }
-
-    //    class RecordButton extends AppCompatButton {
-//        boolean mStartRecording = true;
-//
-//        View.OnClickListener clicker = new View.OnClickListener() {
-//            public void onClick(View v) {
-//                onRecord(mStartRecording);
-//                if (mStartRecording) {
-//                    setText("Stop recording");
-//                } else {
-//                    setText("Start recording");
-//                }
-//                mStartRecording = !mStartRecording;
-//            }
-//        };
-//
-//        public RecordButton(Context ctx) {
-//            super(ctx);
-//            setText("Start recording");
-//            setOnClickListener(clicker);
-//        }
-//    }
 }
